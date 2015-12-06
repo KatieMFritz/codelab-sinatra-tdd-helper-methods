b80332f08bab741757b1039125712a6f0fbd5b8a
Katie Fritz <katie@katiemfritz.com>
2015-12-06 14:29:40 -0500

    image tag method works

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -4,6 +4,9 @@ module MethodsPracticeHelpers
     # Generate an image tag with the given source and the classes
     # "img-responsive" and "img-thumbnail". The image should also have an alt
     # attribute with the contents of alternate_text.
+		%(
+			<img src="#{source}" class="img-responsive img-thumbnail" alt="#{alternate_text}">			
+		)
   end
 
   def bootstrap_alert(message, type='info')
@@ -12,10 +15,10 @@ module MethodsPracticeHelpers
     # the "message" variable and should have a class appropriate for the "type"
     # that is passed in, defaulting to 'info' if no type is passed in.
 		%(
-		<div class="alert alert-#{type} alert-dismissible" role="alert">
-  		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-  		#{message}
-		</div>
+			<div class="alert alert-#{type} alert-dismissible" role="alert">
+	  		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	  		#{message}
+			</div>
 		)
   end
 
